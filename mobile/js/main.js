8980d22e7d7e76f72d2d17d6afe11497db90a70a
olando7 <olandio7@gamil.com>
2019-08-13 16:24:16 +0900

    banner

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,16 +1,27 @@
 $( function(){
    
    $( "#sidemeun_accodion" ).accordion({
-      collapsible: true
-   });
+      heightStyle: "content"
+   }); //sidemenu
  
    $('.hamburger').click(function(){
       $(this).toggleClass('active');
       if($(this).hasClass('active')){
          $('aside').addClass('active');
+         $('body').css({position: 'fixed', top:0, right:0, left:0});
       }else{
          $('aside').removeClass('active');
+         $('body').css({position: 'static'});
       }
-   });
+   }); //hamburger
 
+   $('.search_btn').click(function(ev){
+      ev.preventDefault();
+      $('.main_search_modal').slideToggle();
+   }); //search
+
+   $('.banner_slider').bxSlider({
+      controls:false,
+      auto:true
+   }); //banner
 });
